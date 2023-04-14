1e9700fb6045d1517f898573fa5cc35addc1ecf7
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-04-14 16:55:30 -0400

    update

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const userRouter = require("./routes/users");
 const locationRouter = require("./routes/locations");
 const billRouter = require("./routes/bills");
 const statisticRouter = require("./routes/statistics");
+const mongoose = require("mongoose");
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateI
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected!!!!!!!!");
+});
 
 app.use(express.json());
 
@@ -24,6 +36,9 @@ app.use(statisticRouter);
 //   res.json({ success: true, message: "Welcome to backend zone!" });
 // });
 
-app.listen(5000, () => {
+//STEP 3
+// if(process.env.NODE_)
+
+app.listen(PORT, () => {
   console.log("port is listening");
 });
